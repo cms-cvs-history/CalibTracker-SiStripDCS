c341c6b62e0f0d8cf85ae41da4aa452fd6beb590
Joanne Cole <sha1-fa3e5896900e286b105d4a33c21e2ef8d2c23547@cern.ch>
2008-12-16 11:11:09 +0000

    reverting to previous version of file

---
+++ test/SealModules.cc
@@ -0,0 +1,12 @@
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "CalibTracker/SiStripDCS/test/SiStripModuleHVBuilder.h"
+#include "CalibTracker/SiStripDCS/test/SiStripModuleHVReader.h"
+
+using cms::SiStripModuleHVBuilder;
+using cms::SiStripModuleHVReader;
+
+DEFINE_SEAL_MODULE();
+DEFINE_ANOTHER_FWK_MODULE(SiStripModuleHVBuilder);
+DEFINE_ANOTHER_FWK_MODULE(SiStripModuleHVReader);
+
